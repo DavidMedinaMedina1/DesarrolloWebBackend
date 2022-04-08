b7ba9190c62ea04fde656509663ce44a8366c04d
David Medina <a01653311@tec.mx>
2022-04-07 20:56:33 -0500

    test prueba10

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from crypt import methods
+#1.-
 from datetime import datetime
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import datetime
 # FlASK
 #############################################################
 app = Flask(__name__)
-app.permanent_session_lifetime = datetime.timedelta(days=365)
+#2.-
+app.permanent_session_lifetime = datetime.timedelta(days=1)
+#3.-
 app.secret_key = "super secret key"
 #############################################################
 
@@ -21,11 +24,17 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if (request.method == "GET"):
-        return render_template("Login.html", data="email")
-    else:
-        email = None
-        email = request.form["email"]
-        password = request.form["password"]
-        return render_template("index.html", data=email)
+    email = None
+    if "email" in session:
+        return render_template('index.html', data = session["email"])
+      #  return redirect(url_for("home"))
+
+    else:    
+        if (request.method == "GET"):
+            return render_template("Login.html", data="email")
+        else:
+            email = request.form["email"]
+            password = request.form["password"]
+            session["email"] = email
+            return render_template("index.html", data=email)
 
