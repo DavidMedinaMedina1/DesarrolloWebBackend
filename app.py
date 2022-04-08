ad2dd856204ae695c9fc4a6a4cf858d0c1c7af64
David Medina <a01653311@tec.mx>
2022-04-07 20:44:14 -0500

    test prueba9

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from crypt import methods
-from flask import Flask, render_template, request
-
-
-
+from datetime import datetime
+from flask import Flask, render_template, request, session
+import datetime
 # FlASK
 #############################################################
 app = Flask(__name__)
+app.permanent_session_lifetime = datetime.timedelta(days=365)
+app.secret_key = "super secret key"
 #############################################################
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    email = None
+    if "email" in session:
+        email = session["email"]
+        return render_template('index.html', data = email)
+    else:
+        return render_template('login.html', data = email)
 
 
 @app.route("/login", methods=["GET", "POST"])
