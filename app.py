72a5e7a2cfa01b22dd33450e3af890a807072233
David Medina <a01653311@tec.mx>
2022-04-21 18:53:28 -0500

    Test de /prueba

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 
+from http import client
 from flask import Flask, render_template, request, session, redirect, url_for
 import datetime
+import pymongo
+
 # FlASK
 #############################################################
 app = Flask(__name__)
 app.permanent_session_lifetime = datetime.timedelta(days=1)
 app.secret_key = "super secret key"
 #############################################################
+
+# MONGODB
+#############################################################
+mongodb_key = "mongodb+srv://desarrollowebuser:desarrollowebpassword@cluster0.dfh7g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = pymongo.MongoClient(
+    mongodb_key, tls=True, tlsAllowInvalidCertificates=True)
+db = client.Escuela
+cuentas = db.alumno
+#############################################################
+
+
+
+
+
 
 @app.route('/')
 def home():
@@ -46,3 +63,12 @@ def logout():
     if "email" in session:
         session.clear()
         return redirect(url_for("home"))        
+
+
+@app.route("/usuarios")
+def usuarios():
+    cursor = cuentas.find({})
+    users = []
+    for doc in cursor:
+        users.append(doc)
+    return render_template("/usuarios.html", data=users)
